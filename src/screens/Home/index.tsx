f80581b657b912ecde5b686ac500f3c33218b189
JR DevPM <astronomi@gmail.com>
2022-11-08 18:26:33 -0300

    Created home screen and style

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { Container, Title } from './styles'
 
 export function Home(){
   return (
     <Container>
-
+      <Title>Home</Title>
     </Container>
   )
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -2,4 +2,10 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 16px;
 `;

809da6e910a0aac6c01048d03488bcb281b6bb80
JR DevPM <astronomi@gmail.com>
2022-11-08 23:12:56 -0300

    Import and using ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading'
+import { ThemeProvider } from 'styled-components'
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+
+import theme from './src/styles/theme'
+
 import { Home } from './src/screens/Home';
 
 export default function App() {
-  return (
-    <Home />
-  );
-}
+  const [fontsLoaed] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium
+  });
 
+  if (!fontsLoaed) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  )
+}

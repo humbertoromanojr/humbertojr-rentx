50e0e054e18af20c2edf0afe56fd6cf401e02c30
JR DevPM <astronomi@gmail.com>
2022-11-09 10:05:34 -0300

    Updating Car component

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -1,11 +1,43 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { Container, Details, Brand, Name, 
+  About, Rent, Period, Price, Type, CarImage, CarList 
+} from './styles'
 
-export function Car(){
+import GasolineSvg from '../../assets/gasoline.svg'
+
+interface CarData {
+  brand: string;
+  name: string;
+  rent: {
+    period: string;
+    price: string;
+  },
+  thumbnail: string;
+}
+
+interface Props {
+  data: CarData;
+}
+
+export function Car({ data }: Props){
   return (
     <Container>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>{`RS ${data.rent.price}`}</Price>
+          </Rent>
+          <Type>
+            <GasolineSvg />
+          </Type>
+        </About>
+      </Details>
 
+      <CarImage source={{ uri: data.thumbnail }} resizeMode="contain" />
     </Container>
   )
 }
--- src/components/Car/styles.tsx
+++ src/components/Car/styles.tsx
@@ -1,7 +1,80 @@
-import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native'
+import { FlatList } from 'react-native'
+import { RFValue } from 'react-native-responsive-fontsize'
 
 export const Container = styled.View`
-  flex: 1;
-  background-color: ${({ theme }) => theme.colors.background_primary}
+  width: 100%;
+  height: 126px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 24px;
+  margin-bottom: 16px;
+
+  box-shadow: 1px 1px 5px rgba(125, 123, 123, 0.6);
+  border-radius: 5px;
+
+  background-color: ${({ theme }) => theme.colors.background_secondary}
+`;
+
+export const Details = styled.View`
+
+`;
+export const Brand = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(15)}px;
+`;
+
+export const About = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  margin-top: 16px;
+`;
+
+export const Rent = styled.View`
+margin-right: 24px;
+`;
+
+export const Period = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+`;
+
+export const Price = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  color: ${({ theme }) => theme.colors.main};
+  font-size: ${RFValue(15)}px;
+`;
+
+export const Type = styled.View`
+
+`;
+
+export const CarImage = styled.Image`
+  width: 187px;
+  height: 127px;
+`;
+
+export const CarList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 8
+  },
+  showsVerticalScrollIndicator: false,
+})`
+
 `;

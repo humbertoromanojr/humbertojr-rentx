735732e365336a9ff84d6febf309675f2a25b544
JR DevPM <astronomi@gmail.com>
2022-11-09 13:55:48 -0300

    Finished ImageSlider components

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { CarImage, CarImageWrapper, Container, 
+  ImageIndex, ImageIndexes 
+} from './styles'
 
-export function ImageSlider(){
+interface Props {
+  imagesUrl: string[]
+}
+
+export function ImageSlider({ imagesUrl }: Props){
   return (
     <Container>
+      <ImageIndexes>
+        <ImageIndex active={true} />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+      </ImageIndexes>
 
+      <CarImageWrapper>
+        <CarImage 
+          source={{ uri: imagesUrl[0] }}
+          resizeMode="contain"
+        />
+      </CarImageWrapper>
     </Container>
   )
 }

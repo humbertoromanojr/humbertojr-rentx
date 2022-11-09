a6aadd3e48878413347d291e39a246d94718081b
JR DevPM <astronomi@gmail.com>
2022-11-09 11:19:11 -0300

    Updating BackButton component

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
+import { useTheme } from 'styled-components'
 
 import { Container } from './styles'
 
-export function BackButton(){
-  return (
-    <Container>
+interface Props {
+  color?: string;
+}
 
+export function BackButton({ color, ...rest }: Props){
+  const theme = useTheme()
+
+  return (
+    <Container {...rest}>
+      <MaterialIcons 
+        name="chevron-left" 
+        size={24} 
+        color={color ? color : theme.colors.text} 
+      />
     </Container>
   )
 }
--- src/components/BackButton/styles.tsx
+++ src/components/BackButton/styles.tsx
@@ -1,5 +1,3 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
-  flex: 1;
-`;
+export const Container = styled.View``;

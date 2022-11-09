b6f262a626f99eb7904dbee0b0206e32ee5858e1
JR DevPM <astronomi@gmail.com>
2022-11-09 13:56:17 -0300

    Add ImageSlider component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import { BackButton } from '../../components/BackButton'
 
-import { Container, Header } from './styles'
+import { BackButton } from '../../components/BackButton'
+import { ImageSlider } from '../../components/ImageSlider'
+
+import { Container, Header, CarImages, Content, Details,
+Description, Brand, Name, Rent, Period, Price, About
+} from './styles'
 
 export function CarDetails(){
   return (
@@ -9,6 +13,32 @@ export function CarDetails(){
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider 
+          imagesUrl={['https://pngroyale.com/wp-content/uploads/2022/03/Audi-Car-Free-PNG-Image.png']}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+         <Description>
+          <Brand>Lamborghini</Brand>
+          <Name>Huracan</Name>
+         </Description>
+
+         <Rent>
+          <Period>Ao Dia</Period>
+          <Price>R$ 580</Price>
+         </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de 
+          lide indultado na praça real Maestranza de Sevilla.
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   )
 }

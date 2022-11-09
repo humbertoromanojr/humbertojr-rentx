490fb8627a4b734f7d146e94442f14a00aa1b57c
JR DevPM <astronomi@gmail.com>
2022-11-09 20:47:26 -0300

    Finished Accessory component

--- src/components/Accessory/index.tsx
+++ src/components/Accessory/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { SvgProps } from 'react-native-svg';
 
-import { Container } from './styles'
+import { Container, Name } from './styles'
 
-export function Accessory(){
+interface Props {
+  name: string;
+  icon: React.FC<SvgProps>
+}
+
+export function Accessory({ name, icon: Icon }: Props){
   return (
     <Container>
-
+      <Icon width={32} height={32} />
+      <Name>{name}</Name>
     </Container>
   )
 }
--- src/components/Accessory/styles.tsx
+++ src/components/Accessory/styles.tsx
@@ -1,5 +1,21 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  width: 109px;
+  height: 92px;
+
+  background-color: ${({ theme }) => theme.colors.background_primary};
+
+  padding: 16px;
+  margin-bottom: 8px
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(13)}px;
 `;

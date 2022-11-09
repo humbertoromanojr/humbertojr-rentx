ad6a8883f3f8ec781d32151a4b6819b161bcb0a3
JR DevPM <astronomi@gmail.com>
2022-11-09 08:11:20 -0300

    Created car component

---
+++ src/components/Car/styles.tsx
@@ -0,0 +1,7 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary}
+`;

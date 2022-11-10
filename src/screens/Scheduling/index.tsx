9769c50a1e8a108cc67efe967b50ea85fd7162e1
JR DevPM <astronomi@gmail.com>
2022-11-10 08:38:18 -0300

    Updating with Header and Footer

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,11 +1,60 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
+import { useTheme } from 'styled-components'
 
-import { Container } from './styles'
+import { BackButton } from '../../components/BackButton'
+import { Button } from '../../components/Button'
+
+import { Container, Header, Title, RentalPeriod, DateInfo,
+  DateTitle, DateValue, Content, Footer  
+} from './styles'
+
+import ArrowSvg from '../../assets/arrow.svg'
 
 export function Scheduling(){
+  const theme = useTheme()
+
   return (
     <Container>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent 
+      />
+      <Header>
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+        <Title>
+          Escolha uma {'\n'}
+          data de início e {'\n'}
+          fim do aluguel
+        </Title>
 
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>
+              09/11/2022      
+            </DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>
+              10/11/2022
+            </DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+      
+      </Content> 
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>    
     </Container>
   )
 }

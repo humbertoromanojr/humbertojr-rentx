57092ce42c754f2e8f686956c6bb3f9f7fbfc934
JR DevPM <astronomi@gmail.com>
2022-11-10 22:46:46 -0300

    Finished SchedulingComplete screen

--- src/screens/SchedulingComplete/styles.tsx
+++ src/screens/SchedulingComplete/styles.tsx
@@ -1,5 +1,42 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.header};
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  padding-bottom: 80px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(30)}px;
+
+  text-transform: uppercase;
+
+  margin-top: 40px;
+`;
+
+export const Message = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(15)}px;
+  line-height: 25px;
+  text-align: center;
+
+  margin-top: 16px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  align-items: center;
+
+  margin: 80px 0;
 `;
